019c99cdd2d544c539022b08c6e60017e946fe48
Colton Allen <allen.colton@hotmail.com>
2021-06-01 17:37:11 -0700

    Added The Tipsy Mixer my Projects

--- src/components/About.js
+++ src/components/About.js
@@ -26,7 +26,67 @@ function About({ variants, transitions }) {
       {/* <Canvas /> */}
       <div className="inner">
         <div className="details">
-          <h1 id="about-heading" className="heading">I build functional & progressive websites.</h1>
+          <h1 id="about-heading" className="heading">Front-End ReactJS Web Developer.</h1>
+          <p>Hello, and thank you for checking out my website. My name is Colton Allen, and I'll be your host today. I like coffee, Keanu Reeves movies, long walks to the coffee machine, and…</p>
+          <p>Well, that's what I would say if I was trying to bore you, so I'll get right to the point. I am a passionate web developer with a propensity towards the Front-End Side of development with unrivaled drive and determination who is always striving to improve my skills with every opportunity.</p>
+          <p>I am currently seeking a position as a Front-End Web Developer, and you are probably wondering what technologies I work with? They are as follows:</p>
+          
+          <h2>Base Languages:</h2>
+          <div className="base_langs">
+            <div className="lang_card">
+              <img className="lang_card_img" src="https://img.icons8.com/color/85/000000/html-5.png" alt="HTML5 icon" />
+              <h3>HTML5</h3>
+            </div>
+            <div className="lang_card">
+              <img className="lang_card_img" src="https://img.icons8.com/color/85/000000/css3.png" alt="CSS3 icon" />
+              <h3>CSS3</h3>
+            </div>
+            <div className="lang_card">
+              <img className="lang_card_img" src="https://img.icons8.com/color/85/000000/javascript.png" alt="JavaScript icon" />
+              <h3>JavaScript</h3>
+            </div>
+          </div>
+
+          <h2>Frameworks:</h2>
+          <div className="frameworks">
+            <div className="frame_card">
+              <img className="lang_card_img" src="https://img.icons8.com/color/85/000000/react-native.png" alt="ReactJS icon" />
+              <h3>React</h3>
+            </div>
+            <div className="frame_card">
+              <img className="lang_card_img" src="https://www.markusantonwolf.com/media/pages/blog/tailwind-css/265298487-1596675041/tailwind-css-logo.svg" alt="Tailwind icon" />
+              <h3>Tailwind CSS</h3>
+            </div>
+            <div className="frame_card">
+              <img className="lang_card_img" src="https://img.icons8.com/color/85/000000/sass.png" alt="Sass icon" />
+              <h3>Sass</h3>
+            </div>
+            <div className="frame_card">
+              <img className="lang_card_img invert" src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Nextjs-logo.svg/800px-Nextjs-logo.svg.png" alt="Next.js icon" />
+              <h3>NextJS</h3>
+            </div>
+            <div className="frame_card">
+              <img className="lang_card_img" src="https://img.icons8.com/color/85/000000/typescript.png" alt="TypeScript icon" />
+              <h3>TypeScript</h3>
+            </div>
+            <div className="frame_card">
+              <img className="lang_card_img" src="https://img.icons8.com/color/85/000000/bootstrap.png" alt="Bootstrap icon" />
+              <h3>Bootstrap</h3>
+            </div>
+            <div className="frame_card">
+              <img className="lang_card_img" src="https://img.icons8.com/color/85/000000/nodejs.png" alt="Node.js icon" />
+              <h3>NodeJS</h3>
+            </div>
+          </div>
+
+
+          <ul>
+            <lh className="list_heading">Base Languages:</lh>
+            <li>HTML5</li>
+            <li>CSS3</li>
+            <li>JavaScript</li>
+          </ul>
+
           <p>My name is Colton Allen, and I am first and foremost a Web Developer.</p>
           <p>As an entrepreneur, owning and operating my own business for the past 12 years, I have picked up many skills and learned a lot. In the process, I, of course, had to create a website for my business, and I was captivated ever since.</p>
           <p>Business has been great, but the COVID-19 pandemic truly opened my eyes to the fact that my business was not foolproof. In desperate times, such as they were, business could fall flat in a moment’s time. This got me thinking and formulating another plan; a plan that is more stable for those I love.</p>
